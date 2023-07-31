9f4f77e8f6c5a97c660960070eef679ce9007d9c
mdowns <michaeljdowns99@gmail.com>
2023-07-31 14:23:00 -0600

    Test Commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 mongoose
 
-  .connect(process.env.MONGODB_URI)
+  .connect("mongodb+srv://mdowns:Km64JrPLuZVXjpXw@cluster0.729um46.mongodb.net/todoDB")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Connection failed: " + err));
   
